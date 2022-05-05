f9d4c036055fa71e82bd679907118b32b5b9dad4
G Jones <jonesgrega@gmail.com>
2022-05-04 22:02:45 -0600

    changing to master branch again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Here is my edited text.. to test action
+          Here is my edited text.. to test action2
         </p>
         <a
           className="App-link"
